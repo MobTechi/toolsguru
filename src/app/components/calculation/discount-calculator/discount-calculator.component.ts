f4ea49f76c950b637f5b3f14d90a8dce122df598
Poovarasan <poovarasan@cloudrippless-MacBook-Air.local>
2023-10-28 15:24:00 +0530

    Fixed the discount calculator

--- src/app/components/calculation/discount-calculator/discount-calculator.component.ts
+++ src/app/components/calculation/discount-calculator/discount-calculator.component.ts
@@ -1,14 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-discount-calculator',
   templateUrl: './discount-calculator.component.html',
   styleUrls: ['./discount-calculator.component.scss'],
 })
-export class DiscountCalculatorComponent  implements OnInit {
+export class DiscountCalculatorComponent {
+
+  public originalPrice!: number;
+  public discountPercent!: number;
+  public discountedPrice!: number;
+  public savedAmount!: number;
 
   constructor() { }
 
-  ngOnInit() {}
+  public calculateDiscount() {
+    if (this.originalPrice && this.discountPercent) {
+      this.savedAmount = (this.originalPrice * this.discountPercent) / 100;
+      this.discountedPrice = this.formatPrice(this.originalPrice - this.savedAmount);
+      this.savedAmount = this.formatPrice(this.savedAmount);
+    }
+  }
 
+  formatPrice(price: number) {
+    // Removes trailing zeros and decimals
+    return Number(price.toFixed(2).replace(/(\.0*|0*)$/, ''));
+  }
 }
